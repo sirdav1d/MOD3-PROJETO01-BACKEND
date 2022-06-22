4af12d63e9484df88be2fd3a0c6875e9d97b9f90
sirdav1d <ddavid.diniz@gmail.com>
2022-06-21 21:53:07 -0300

    feat crud

--- src/controllers/paletas.controller.js
+++ src/controllers/paletas.controller.js
@@ -11,7 +11,29 @@ const findPaletaByIdController = (req, res) => {
   res.send(chosenPaleta);
 };
 
+const createPaletaController = (req, res) => {
+  const paleta = req.body;
+  const newPaleta = paletasService.createPaletaService(paleta);
+  res.send(newPaleta);
+};
+
+const updatePaletaController = (req, res) => {
+  const idParam = +req.params.id;
+  const paletaEdit = req.body;
+  const updatedPaleta = paletasService.updatePaletaService(idParam, paletaEdit);
+  res.send(updatedPaleta);
+};
+
+const deletePaletaController = (req, res) => {
+  const idParam = req.params.id;
+  paletasService.deletePaletaService(idParam);
+  res.send({ message: 'Paleta deletada com sucesso!' });
+};
+
 module.exports = {
   findPaletasController,
   findPaletaByIdController,
+  createPaletaController,
+  updatePaletaController,
+  deletePaletaController,
 };
--- src/services/paletas.service.js
+++ src/services/paletas.service.js
@@ -33,7 +33,29 @@ const findPaletaByIdService = (id) => {
   return paletas.find((paleta) => paleta.id == id);
 };
 
+const createPaletaService = (newPaleta) => {
+  const newId = paletas.length + 1;
+  newPaleta.id = newId;
+  paletas.push(newPaleta);
+  return newPaleta;
+};
+
+const updatePaletaService = (id, paletaEdited) => {
+  paletaEdited['id'] = id;
+  const paletaIndex = paletas.findIndex((paleta) => paleta.id == id);
+  paletas[paletaIndex] = paletaEdited;
+  return paletaEdited;
+};
+
+const deletePaletaService = (id) => {
+  const paletaIndex = paletas.findIndex((paleta) => paleta.id == id);
+  return paletas.splice(paletaIndex, 1);
+};
+
 module.exports = {
   findPaletasService,
   findPaletaByIdService,
+  createPaletaService,
+  updatePaletaService,
+  deletePaletaService,
 };
